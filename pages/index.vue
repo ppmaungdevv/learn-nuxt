03d03efcc9103e1bb47bcbb6a1b0854df65d2ad0
ppmaungdev <ppmaungdev@gmail.com>
2022-09-01 20:03:04 +0630

    fixed tailwind problem

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+<div class="card">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem voluptas hic, expedita vero reiciendis recusandae excepturi culpa placeat voluptatibus eligendi eos labore corrupti quaerat quo beatae voluptates repellat repudiandae. Dignissimos?</div>
 </template>
 
 <script>
