0f810278755532b8aa681a8bc3cf47cc8f7c9573
Ko Pyae <kopyae@Kos-Mac-mini.local>
2022-09-11 10:17:58 +0630

    axios and fetch hook

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,27 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      currencies: {},
+      api_route: 'https://api.exchangerate.host/latest'
+    }
+  },
+  async fetch() {
+    await this.getCurrencies()
+  },
+  // mounted(){ 
+  //   this.fetchCurrencies()
+  // },
+  methods: {
+    async getCurrencies() {
+      const { data } = await axios.get(this.api_route)
+      this.currencies = JSON.parse(JSON.stringify(data.rates))
+      console.log('-------', this.currencies)
+    }
+  }
 }
 </script>
