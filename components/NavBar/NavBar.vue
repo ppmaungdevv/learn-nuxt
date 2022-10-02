baebe74a38c9af506aa8054f2c9af368f0276221
ppmaungdev <ppmaungdev@gmail.com>
2022-10-02 11:46:20 +0630

    separate component and add custom events for search box

--- components/NavBar/NavBar.vue
+++ components/NavBar/NavBar.vue
@@ -1,6 +1,16 @@
 <template src='./NavBar.html' />
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
+    data() {
+        return {
+            search_data: '',
+        }
+    },
+    methods: {
+        test() {
+            console.log(this.search_data)
+        }
+    },
 }
 </script>
---
+++ components/SearchBox/SearchBox.vue
@@ -0,0 +1,17 @@
+<template src="./SearchBox.html" />
+
+<script>
+export default {
+    name: 'SearchBox',
+    props: ['value'],
+    methods: {
+        emitEvent(event_name){
+            this.$emit(event_name)
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
