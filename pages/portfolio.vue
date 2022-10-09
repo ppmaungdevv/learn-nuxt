11d37ee9a385b880643b740720c20b7ed06d2674
ppmaungdev <ppmaungdev@gmail.com>
2022-10-09 14:51:40 +0630

    init portfolio

---
+++ pages/portfolio.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <div class="min-h-screen p-4 text-teal-400 bg-gradient-to-br from-[#292e49] via-[#536976] to-[#bbd2c5]"> -->
+  <div class="min-h-screen p-4 bg-gradient-to-br from-[#afc9dc] to-[#f2f4f6] text-[#2b2b2b]">
+    <!-- f6f6f6, f2f4f6, 303030 -->
+    <!-- font-color => 303030, 2b2b2b, 1b1c1e, afc9dc -->
+    <!-- <h1 class=" text-5xl">Who am I ?</h1> -->
+    <div class="container mx-auto my-5">
+      <div class="flex items-center justify-center p-5">
+          <img class="h-44 w-44 rounded-full border shadow-lg" src="../assets/img/PPM_CU_1.png" alt="ppm_cu_img">
+      </div>
+      <div class="flex items-center justify-center p-5">
+        <div class="text-left">
+          <p class="text-xl p-2">Hi. I'm Pyae Phyo Maung</p>
+          <p class="text-xl p-2">I've been working as a developer for 4 years</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'portfolio'
+}
+</script>
+
+<style>
+</style>
