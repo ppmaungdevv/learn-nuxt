4ea4a11a3a6c97aca62120a4df91e1fce92c059e
ppmaungdev <ppmaungdev@gmail.com>
2022-11-05 19:16:58 +0630

    detail btn route binding

--- pages/_id/detail/index.vue
+++ pages/_id/detail/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <p>test detail</p>
+  <div class="container mx-auto text-white">
+    <button @click="$router.back()">back</button>
+    <p>test detail</p>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'DetailPage'
+  name: 'DetailPage'
 }
 </script>
 
